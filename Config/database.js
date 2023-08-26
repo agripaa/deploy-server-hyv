6996947439a0ce669f2ed117bc609753c89b8323
Jounnyy <agrieva.xananda08@gmail.com>
2023-08-26 07:55:21 +0700

    add dialect variable

--- Config/database.js
+++ Config/database.js
@@ -2,8 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect: process.env.DATABASE,
+    dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
 });
 
